c03f8326e38a6a9ac4877bc377298e233ac12047
REX <cssmith431@yahoo.com>
2022-02-10 12:37:20 -0500

    questions

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+const inquirer = require("inquirer");
+console.log(inquirer);
 
-const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer.prompt ([
+          {
+            type: "input",
+            name: "name",
+            message: "what is your name?",
+          }
 
-const [name, github] = profileDataArgs;
+]).then(answers => console.log(answers));
 
-const generatePage = require('./src/page-template.js');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+
+//const pageHTML = generatePage(name, github);
+
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
